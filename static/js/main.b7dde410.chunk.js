362610fb48f61c6d9ede860c221fc427e7b22169
vray797 <vray797@gmail.com>
2021-07-19 16:52:24 +0530

    Updates

--- static/js/main.b7dde410.chunk.js
+++ static/js/main.b7dde410.chunk.js
@@ -1,2 +1,2 @@
 (this.webpackJsonppersonal_dashboard=this.webpackJsonppersonal_dashboard||[]).push([[0],{11:function(n,e,t){"use strict";t.r(e);var c=t(1),a=t.n(c),i=t(3),o=t.n(i),r=(t(8),t(9),t(0));var s=function(){return Object(r.jsx)("div",{className:"App",children:Object(r.jsx)("h1",{children:"Financial Dashboard"})})},d=function(n){n&&n instanceof Function&&t.e(3).then(t.bind(null,12)).then((function(e){var t=e.getCLS,c=e.getFID,a=e.getFCP,i=e.getLCP,o=e.getTTFB;t(n),c(n),a(n),i(n),o(n)}))};o.a.render(Object(r.jsx)(a.a.StrictMode,{children:Object(r.jsx)(s,{})}),document.getElementById("root")),d()},8:function(n,e,t){},9:function(n,e,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.5ff2db1a.chunk.js.map
+//# sourceMappingURL=main.b7dde410.chunk.js.map
